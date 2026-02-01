e01549bb18c617afac63cd44c35f18045fce79a6
cameronh3d2002-star <cameronh3d.2002@gmail.com>
2026-02-01 21:35:37 +0000

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+});
+
+mobileMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("open");
+});
